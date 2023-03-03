b77e3ce6d4ebe33b297968e50593bbe1a64b5ff7
deborabcarvalho <debora.carvalho@FCBM-321293.local>
2023-03-03 12:45:07 -0300

    commit inicial

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-const form = document.getElementById(`form-atividade`)
-const imgAprovado = '<img src="./images/aprovado.png" alt="Emoje celebrando" />'
-const imgReprovado = '<img src="./images/reprovado.png" alt="Emoje decepcionado" />'
-const atividades = []
-const notas = []
-const spanAprovado = `<span class = "resultado aprovado">Aprovado</span>`
-const spanReprovado = `<span class = "resultado reprovado">Reprovado</span>`
-const notaMinima = parseFloat(prompt("Digite a nota mínima:"))
+const form = document.getElementById(`form-agenda`)
+const Nome = []
+const Tel = []
 
 let linhaS = ''
 
@@ -14,53 +9,33 @@ form.addEventListener('submit', function(e){
 
     adicionaLinha()
     atualizaTabela()
-    atualizaMediaFinal()
-})
+
+   })
 
 function adicionaLinha(){
-    const inputNomeAtividade = document.getElementById('nome-atividade')
-    const inputNotaAtividade = document.getElementById('nota-atividade')
+    const inputNomeContato = document.getElementById('nome-contato')
+    const inputTELContato = document.getElementById('fone-contato')
 
-    if(atividades.includes(inputNomeAtividade.value)){
-        alert(`A atividade ${inputNomeAtividade.value} já foi inserida`)
+    if(Nome.includes(inputNomeContato.value)){
+        alert(`A atividade ${inputNomeContato.value} já foi inserida`)
       } else {
-        atividades.push(inputNomeAtividade.value)
-        notas.push(parseFloat(inputNotaAtividade.value))
+        Nome.push(inputNomeContato.value)
+        Tel.push(parseFloat(inputTELContato.value))
 
         let linha =`<tr>`
-        linha += `<td>${inputNomeAtividade.value}</td>`
-        linha += `<td>${inputNotaAtividade.value}</td>`
-        linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`
+        linha += `<td>${inputNomeContato.value}</td>`
+        linha += `<td>${inputTELContato.value}</td>`
         linha += `</tr>`
 
         linhaS += linha
     }
 
-    inputNomeAtividade.value = ' '
-    inputNotaAtividade.value = ' '
+    inputNomeContato.value = ' '
+    inputTELContato.value = ' '
 }
 
 
 function atualizaTabela(){
     const corpoTabela = document.querySelector('tbody')
     corpoTabela.innerHTML = linhaS
-}
-
-
-function calculaMediaFinal(){
-    let somaDasNotas = 0
-
-    for (let i = 0; i < notas.length; i++){
-        somaDasNotas += notas[i]
-    }
-
-    return somaDasNotas / notas.length
-}
-
-
-function atualizaMediaFinal(){
-    const mediaFinal = calculaMediaFinal()
-
-    document.getElementById(`media-final-valor`).innerHTML = mediaFinal.toFixed(2)
-    document.getElementById(`media-final-resultado`).innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
